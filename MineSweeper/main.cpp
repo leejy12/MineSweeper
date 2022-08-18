bcc4092db34bc28f49b8516b09b076babba5a6b3
leejy12 <leejy31415@gmail.com>
2022-08-19 02:04:04 +0900

    Simplify config file handling

--- MineSweeper/main.cpp
+++ MineSweeper/main.cpp
@@ -1,7 +1,7 @@
 #include <Windows.h>
 #include <array>
-#include <strsafe.h>
-#include <sstream>
+#include <fstream>
+#include <filesystem>
 
 import MineSweeperGame;
 
@@ -9,52 +9,31 @@ int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 {
     // Open the config file stored in the AppData folder
     wchar_t configFilePath[256] = { 0 };
-    ExpandEnvironmentStringsW(L"%USERPROFILE%", configFilePath, 32);
-    StringCchCatW(configFilePath, 256, L"\\AppData\\Local\\MineSweeper\\config.txt");
-    bool madeNewFile = false;
+    wchar_t configFileDir[256] = { 0 };
+    ExpandEnvironmentStringsW(L"%USERPROFILE%\\AppData\\Local\\MineSweeper\\config.txt", configFilePath, 256);
+    ExpandEnvironmentStringsW(L"%USERPROFILE%\\AppData\\Local\\MineSweeper", configFileDir, 256);
 
-    HANDLE hConfigFile = CreateFileW(configFilePath,
-                                     GENERIC_READ | GENERIC_WRITE,
-                                     FILE_SHARE_READ | FILE_SHARE_WRITE,
-                                     nullptr,
-                                     OPEN_EXISTING,
-                                     FILE_ATTRIBUTE_NORMAL,
-                                     nullptr);
-
-    DWORD dwError = GetLastError();
-
-    // If the configFile failed to open (probably because the file does not exist, create it.
-    if (hConfigFile == INVALID_HANDLE_VALUE)
-    {
-        // Ensure that AppData\Local\MineSweeper directory exists.
-        if (dwError == ERROR_PATH_NOT_FOUND)
-        {
-            wchar_t configFileDir[256] = { 0 };
-            ExpandEnvironmentStringsW(L"%USERPROFILE%", configFileDir, 32);
-            StringCchCatW(configFileDir, 256, L"\\AppData\\Local\\MineSweeper");
-            CreateDirectoryW(configFileDir, nullptr);
-        }
-        hConfigFile = CreateFileW(configFilePath,
-                                  GENERIC_READ | GENERIC_WRITE,
-                                  FILE_SHARE_READ | FILE_SHARE_WRITE,
-                                  nullptr,
-                                  CREATE_NEW,
-                                  FILE_ATTRIBUTE_NORMAL,
-                                  nullptr);
-        madeNewFile = true;
-    }
-
-    char configBuffer[16] = { 0 };
+    std::ifstream inConfigFile(configFilePath);
+    std::ofstream outConfigFile;
     int width, height, mines;
-    bool wasParseSuccessful = true;
+    bool wasParseSuccessful = true, madeNewFile = false;
 
-    // If the file exists, read from it and parse it to initialize widht, height, mines.
-    if (!madeNewFile)
+    if (inConfigFile.is_open())
     {
-        ReadFile(hConfigFile, configBuffer, sizeof(configBuffer), nullptr, nullptr);
-        std::stringstream ss(configBuffer);
-        wasParseSuccessful = static_cast<bool>(ss >> width >> height >> mines);
+        wasParseSuccessful = static_cast<bool>(inConfigFile >> width >> height >> mines);
+        inConfigFile.close();
     }
+    else
+    {
+        if (!std::filesystem::exists(configFileDir))
+        {
+            std::filesystem::create_directory(configFileDir);
+        }
+        outConfigFile.open(configFilePath);
+        madeNewFile = true;
+        outConfigFile.close();
+    }
+
     // If the parsing was unsuccessful or we created a new file (empty), then initialize
     // width, height, mines to Easy difficulty values.
     if (!wasParseSuccessful || madeNewFile)
@@ -72,21 +51,8 @@ int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 
     // Close and reopen the config file, but in write mode this time, to erase the contents.
     // Then, write to the file the final game configurations.
-    CloseHandle(hConfigFile);
-    hConfigFile = CreateFileW(configFilePath,
-                              GENERIC_WRITE,
-                              FILE_SHARE_READ | FILE_SHARE_WRITE,
-                              nullptr,
-                              TRUNCATE_EXISTING,
-                              FILE_ATTRIBUTE_NORMAL,
-                              nullptr);
-
-    ZeroMemory(configBuffer, sizeof(configBuffer));
-    StringCbPrintfA(configBuffer, sizeof(configBuffer), "%d %d %d", finalWidth, finalHeight, finalMines);
-
-    DWORD dwWritten;
-    WriteFile(hConfigFile, configBuffer, sizeof(configBuffer), &dwWritten, nullptr);
-    CloseHandle(hConfigFile);
+    outConfigFile.open(configFilePath);
+    outConfigFile << finalWidth << ' ' << finalHeight << ' ' << finalMines;
 
     return 0;
 }
